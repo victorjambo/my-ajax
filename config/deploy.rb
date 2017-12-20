7c088c009a0de76a95ece32c3eadd3ba4288fe13
victorjambo <victorjambo@live.com>
2017-12-20 12:43:14 +0300

    linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,6 +66,21 @@ namespace :deploy do
     end
   end
 
+  task :copy_config do
+    on release_roles :app do |role|
+      fetch(:linked_files).each do |linked_file|
+        user = role.user + "@" if role.user
+        hostname = role.hostname
+        linked_files(shared_path).each do |file|
+          run_locally do
+            execute :rsync, "config/#{file.to_s.gsub(/.*\/(.*)$/,"\\1")}", "#{user}#{hostname}:#{file.to_s.gsub(/(.*)\/[^\/]*$/, "\\1")}/"
+          end
+        end
+      end
+    end
+  end
+  before "deploy:check:linked_files", "deploy:copy_config"
+
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
