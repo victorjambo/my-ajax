931d03cdc9f1f6a128a86ffa437d5d733f244ba1
victorjambo <victorjambo@live.com>
2017-12-20 22:13:45 +0300

    pg

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,85 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.1"
-set :repo_url, 'git@github.com:victorjambo/my-ajax.git'
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-set :user, 'victormutaijambo'
-set :application, 'my-ajax'
-set :rails_env, 'production'
-server '35.196.22.156', user: "#{fetch(:user)}", roles: %w{app db web}, primary: true
+# Change these
+server '35.196.22.156', port: 22, roles: [:web, :app, :db], primary: true
+
+set :repo_url,        'git@github.com:victorjambo/my-ajax.git'
+set :application,     'my-ajax'
+set :user,            'victormutaijambo'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :migration_role,  :attendance_tracker
+set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :pty, true
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :rollbar_token, '1a1ae3c240bc4d978055d92466c2a8f9'
+set :rollbar_env, proc { fetch :stage }
+set :rollbar_role, proc { :app }
 
-set :config_example_suffix, '.example'
-set :config_files, %w{config/database.yml config/secrets.yml}
-set :puma_conf, "#{shared_path}/config/puma.rb"
+## Defaults:
+# set :scm,           :git
+# set :branch,        :master
+# set :format,        :pretty
+# set :log_level,     :debug
+# set :keep_releases, 5
+
+## Linked Files & Directories (Default None):
+set :linked_files, %w{config/puma.rb config/database.yml config/secrets.yml}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
 
 namespace :deploy do
-  before 'check:linked_files', 'config:push'
-  before 'check:linked_files', 'puma:config'
-  before 'check:linked_files', 'puma:nginx_config'
-  before 'deploy:migrate', 'deploy:db:create'
-  after 'puma:smart_restart', 'nginx:restart'
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
 end
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
